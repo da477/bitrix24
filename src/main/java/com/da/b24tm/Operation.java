695abf1dd15db42ef5b3e1a272748974839ce268
da477 <da.1cint.eu@gmail.com>
2024-12-27 15:22:48 -0600

    add lombok to project

--- src/main/java/com/da/b24tm/Operation.java
+++ src/main/java/com/da/b24tm/Operation.java
@@ -1,15 +1,29 @@
 package com.da.b24tm;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+@Getter
 public enum Operation {
-	OPENED("open"), PAUSED("pause"), CLOSED("close"), STATUS("status"), AUTO("auto"), EXIT("exit");
+    OPENED("open"),
+    PAUSED("pause"),
+    CLOSED("close"),
+    STATUS("status"),
+    AUTO("auto"),
+    EXIT("exit");
 
-	private final String value;
+    private final String value;
 
-	Operation(String value) {
-		this.value = value;
-	}
+    Operation(String value) {
+        this.value = value;
+    }
 
-	public String getValue() {
-		return value;
-	}
+    @JsonCreator
+    public static Operation fromString(String str) {
+        try {
+            return Operation.valueOf(str.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid Operation name: " + str);
+        }
+    }
 }
--- src/main/java/com/da/b24tm/command/OpenCommand.java
+++ src/main/java/com/da/b24tm/command/OpenCommand.java
@@ -2,15 +2,16 @@ package com.da.b24tm.command;
 
 import com.da.b24tm.ConsoleHelper;
 import com.da.b24tm.HttpCon;
-import com.da.b24tm.Operation;
+
+import static com.da.b24tm.Operation.OPENED;
 
 public class OpenCommand implements Command {
 	@Override
 	public String execute() throws Exception {
 
-		ConsoleHelper.writeMessage("Send: " + Operation.OPENED);
+		ConsoleHelper.writeMessage("Send: " + OPENED);
 
-		HttpCon.getInstance().getHttpConnection(Operation.OPENED.getValue());
+		HttpCon.getInstance().getHttpConnection(OPENED.getValue());
 		HttpCon.getInstance().analiseResponseCode();
 
 		return "";
